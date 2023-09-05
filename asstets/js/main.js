0df3cf0a38fe99055eff6914afd119438aafb79c
rpsobolewski <rpsobolewski@gmail.com>
2023-09-05 17:56:56 +0200

    added time
    two dates, difference between dates is time in ms, converted to sec

--- asstets/js/main.js
+++ asstets/js/main.js
@@ -2,15 +2,30 @@
 Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di domani mattina!
  */
 
+// oggi
+const ora = new Date();
+console.log(ora);
 
-let seconds = 10;
+// domani alle 9 30
+const domani = new Date();
+domani.setDate(ora.getDate() + 1);
+domani.setHours(9, 30, 0, 0); 
+console.log(domani);
+
+// differenza di tempo in ms?
+const tempo = domani - ora;
+console.log(tempo);
+
+
+
+let seconds = Math.floor(tempo / 1000);
 
 const clock = setInterval(function () {
   document.querySelector(".countdown").innerHTML = seconds;
 
   if (seconds == 0) {
     clearInterval(clock);
-    alert("Buon anno!");
+    alert("ao");
   } else {
     seconds--;
   }
